ce532a5ef02beb44c37cf346eabda649d606941a
dkuryakov <fifer2@yandex.ru>
2022-02-12 22:14:10 +0700

    feat: Создание компонента OfferScreen

--- project/src/components/screens/offer-screen/offer-screen.tsx
+++ project/src/components/screens/offer-screen/offer-screen.tsx
@@ -104,12 +104,8 @@ function OfferScreen(): JSX.Element {
                 <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
                   <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar" />
                 </div>
-                <span className="property__user-name">
-                    Angelina
-                  </span>
-                <span className="property__user-status">
-                    Pro
-                  </span>
+                <span className="property__user-name">Angelina</span>
+                <span className="property__user-status">Pro</span>
               </div>
               <div className="property__description">
                 <p className="property__text">
@@ -130,9 +126,7 @@ function OfferScreen(): JSX.Element {
                     <div className="reviews__avatar-wrapper user__avatar-wrapper">
                       <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
                     </div>
-                    <span className="reviews__user-name">
-                        Max
-                      </span>
+                    <span className="reviews__user-name">Max</span>
                   </div>
                   <div className="reviews__info">
                     <div className="reviews__rating rating">
@@ -152,43 +146,37 @@ function OfferScreen(): JSX.Element {
                 <label className="reviews__label form__label" htmlFor="review">Your review</label>
                 <div className="reviews__rating-form form__rating">
                   <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-                    <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
+                  <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
+                    <svg className="form__star-image" width="37" height="33"><use xlinkHref="#icon-star" /></svg>
+                  </label>
 
-                    <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-                      <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                        <svg className="form__star-image" width="37" height="33">
-                          <use xlinkHref="#icon-star" />
-                        </svg>
-                      </label>
+                  <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
+                  <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
+                    <svg className="form__star-image" width="37" height="33">
+                      <use xlinkHref="#icon-star" />
+                    </svg>
+                  </label>
 
-                      <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-                        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                          <svg className="form__star-image" width="37" height="33">
-                            <use xlinkHref="#icon-star" />
-                          </svg>
-                        </label>
+                  <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
+                  <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
+                    <svg className="form__star-image" width="37" height="33">
+                      <use xlinkHref="#icon-star" />
+                    </svg>
+                  </label>
 
-                        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-                          <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                            <svg className="form__star-image" width="37" height="33">
-                              <use xlinkHref="#icon-star" />
-                            </svg>
-                          </label>
+                  <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
+                  <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
+                    <svg className="form__star-image" width="37" height="33">
+                      <use xlinkHref="#icon-star" />
+                    </svg>
+                  </label>
 
-                          <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-                            <label htmlFor="1-star" className="reviews__rating-label form__rating-label"
-                                   title="terribly">
-                              <svg className="form__star-image" width="37" height="33">
-                                <use xlinkHref="#icon-star" />
-                              </svg>
-                            </label>
+                  <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
+                  <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
+                    <svg className="form__star-image" width="37" height="33"><use xlinkHref="#icon-star" /></svg>
+                  </label>
                 </div>
-                <textarea className="reviews__textarea form__textarea" id="review" name="review"
-                          placeholder="Tell how was your stay, what you like and what can be improved" />
+                <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" />
                 <div className="reviews__button-wrapper">
                   <p className="reviews__help">
                     To submit review please make sure to set <span className="reviews__star">rating</span> and describe
@@ -218,8 +206,7 @@ function OfferScreen(): JSX.Element {
                     <b className="place-card__price-value">&euro;80</b>
                     <span className="place-card__price-text">&#47;&nbsp;night</span>
                   </div>
-                  <button className="place-card__bookmark-button place-card__bookmark-button--active button"
-                          type="button">
+                  <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
                     <svg className="place-card__bookmark-icon" width="18" height="19">
                       <use xlinkHref="#icon-bookmark" />
                     </svg>
