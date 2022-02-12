6691a29d16b3afb57c841e65ef307a37040a412e
dkuryakov <fifer2@yandex.ru>
2022-02-12 22:24:18 +0700

    feat: Создание компонента NotLoggedScreen

--- project/src/components/screens/not-logged-offer-screen/not-logged-offer-screen.tsx
+++ project/src/components/screens/not-logged-offer-screen/not-logged-offer-screen.tsx
@@ -126,9 +126,7 @@ function NotLoggedOfferScreen(): JSX.Element {
                     <div className="reviews__avatar-wrapper user__avatar-wrapper">
                       <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
                     </div>
-                    <span className="reviews__user-name">
-                        Max
-                      </span>
+                    <span className="reviews__user-name">Max</span>
                   </div>
                   <div className="reviews__info">
                     <div className="reviews__rating rating">
@@ -165,8 +163,7 @@ function NotLoggedOfferScreen(): JSX.Element {
                     <b className="place-card__price-value">&euro;80</b>
                     <span className="place-card__price-text">&#47;&nbsp;night</span>
                   </div>
-                  <button className="place-card__bookmark-button place-card__bookmark-button--active button"
-                          type="button">
+                  <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
                     <svg className="place-card__bookmark-icon" width="18" height="19">
                       <use xlinkHref="#icon-bookmark" />
                     </svg>
