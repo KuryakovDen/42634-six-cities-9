978433595b0175327fdb5f942f3408dcfd2a9525
dkuryakov <fifer2@yandex.ru>
2022-02-15 21:41:57 +0700

    feat: Добавление маршрутизации

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,27 @@
 import MainScreen from '../../screens/main-screen/main-screen';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import LoginScreen from '../../screens/login-screen/login-screen';
+import FavoritesScreen from '../../screens/favorites-screen/favorites-screen';
+import OfferScreen from '../../screens/offer-screen/offer-screen';
+import NotFoundScreen from '../../screens/not-found-screen/not-found-screen';
 
 type AppProps = {
   placesCount: number;
 };
 
 function App({ placesCount }: AppProps): JSX.Element {
-  return <MainScreen placesCount={placesCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainScreen placesCount={placesCount} />} />
+        <Route path={AppRoute.SignIn} element={<LoginScreen />} />
+        <Route path={AppRoute.Favorites} element={<FavoritesScreen />} />
+        <Route path={AppRoute.Offer} element={<OfferScreen />} />
+        <Route path={'*'} element={<NotFoundScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
