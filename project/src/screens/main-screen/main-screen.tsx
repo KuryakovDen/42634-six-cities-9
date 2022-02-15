ebf30ee956bbfcb1174f1bb80b57d1edfada0dba
dkuryakov <fifer2@yandex.ru>
2022-02-15 22:36:38 +0700

    refactor: Выделение компонента LocationTabs

--- project/src/screens/main-screen/main-screen.tsx
+++ project/src/screens/main-screen/main-screen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import OfferCard from '../../components/offer-card/offer-card';
 import Header from '../../components/header/header';
+import LocationTabs from '../../components/location-tabs/location-tabs';
 
 type MainScreenProps = {
   placesCount: number;
@@ -13,42 +14,8 @@ function MainScreen({ placesCount }: MainScreenProps): JSX.Element {
 
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active">
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
-          </section>
-        </div>
+        <LocationTabs />
+
         <div className="cities">
           <div className="cities__places-container container">
             <section className="cities__places places">
