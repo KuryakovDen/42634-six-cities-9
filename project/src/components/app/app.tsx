db4b1983adf0b37e2048a37bcf405f8323fc77d1
dkuryakov <fifer2@yandex.ru>
2022-02-15 23:08:01 +0700

    feat: Оформление страницы Favorites в качетсве приватного маршрута

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,9 @@
 import MainScreen from '../../screens/main-screen/main-screen';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import {AppRoute, AuthStatus} from '../../const';
 import LoginScreen from '../../screens/login-screen/login-screen';
 import FavoritesScreen from '../../screens/favorites-screen/favorites-screen';
+import PrivateRoute from '../private-route/private-route';
 import OfferScreen from '../../screens/offer-screen/offer-screen';
 import NotFoundScreen from '../../screens/not-found-screen/not-found-screen';
 
@@ -16,7 +17,7 @@ function App({ placesCount }: AppProps): JSX.Element {
       <Routes>
         <Route path={AppRoute.Main} element={<MainScreen placesCount={placesCount} />} />
         <Route path={AppRoute.Login} element={<LoginScreen />} />
-        <Route path={AppRoute.Favorites} element={<FavoritesScreen />} />
+        <Route path={AppRoute.Favorites} element={<PrivateRoute authStatus={AuthStatus.NoAuth}><FavoritesScreen /></PrivateRoute>} />
         <Route path={AppRoute.Offer} element={<OfferScreen />} />
         <Route path={'*'} element={<NotFoundScreen />} />
       </Routes>
--- project/src/screens/login-screen/login-screen.tsx
+++ project/src/screens/login-screen/login-screen.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
 
 function LoginScreen(): JSX.Element {
   return (
@@ -7,9 +9,9 @@ function LoginScreen(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
+              <Link className="header__logo-link" to={AppRoute.Main}>
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              </Link>
             </div>
           </div>
         </div>
