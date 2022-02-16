112efad666063b20e6658f91824d4bb1cc5eb0b4
dkuryakov <fifer2@yandex.ru>
2022-02-16 23:17:19 +0700

    refactor: Выделение компонентов ReviewForm

--- project/src/screens/offer-screen/offer-screen.tsx
+++ project/src/screens/offer-screen/offer-screen.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Header from '../../components/header/header';
-import OfferCard from '../../components/offer-card/offer-card';
 import ReviewsList from '../../components/reviews-list/reviews-list';
+import ReviewForm from '../../components/review-form/review-form';
+import OffersList from '../../components/offers-list/offers-list';
 
 function OfferScreen(): JSX.Element {
   return (
@@ -126,49 +127,7 @@ function OfferScreen(): JSX.Element {
               </div>
               <section className="property__reviews reviews">
                 <ReviewsList />
-                <form className="reviews__form form" action="#" method="post">
-                  <label className="reviews__label form__label" htmlFor="review">Your review</label>
-                  <div className="reviews__rating-form form__rating">
-                    <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-                    <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-                      <svg className="form__star-image" width="37" height="33"><use xlinkHref="#icon-star" /></svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-                    <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-                    <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-                    <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-                      <svg className="form__star-image" width="37" height="33">
-                        <use xlinkHref="#icon-star" />
-                      </svg>
-                    </label>
-
-                    <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-                    <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-                      <svg className="form__star-image" width="37" height="33"><use xlinkHref="#icon-star" /></svg>
-                    </label>
-                  </div>
-                  <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" />
-                  <div className="reviews__button-wrapper">
-                    <p className="reviews__help">
-                      To submit review please make sure to set <span className="reviews__star">rating</span> and describe
-                      your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                    </p>
-                    <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-                  </div>
-                </form>
+                <ReviewForm />
               </section>
             </div>
           </div>
@@ -177,11 +136,7 @@ function OfferScreen(): JSX.Element {
         <div className="container">
           <section className="near-places places">
             <h2 className="near-places__title">Other places in the neighbourhood</h2>
-            <div className="near-places__list places__list">
-              <OfferCard image={'img/apartment-02.jpg'} price={100} />
-              <OfferCard image={'img/apartment-03.jpg'} price={300} />
-              <OfferCard image={'img/apartment-01.jpg'} price={55} />
-            </div>
+            <OffersList />
           </section>
         </div>
       </main>
