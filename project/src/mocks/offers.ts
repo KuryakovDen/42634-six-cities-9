5a9337aa8850ec2254c41a544d6f416647ef669f
dkuryakov <fifer2@yandex.ru>
2022-02-21 22:49:46 +0700

    feat: Создание моков для предложений

---
+++ project/src/mocks/offers.ts
@@ -0,0 +1,154 @@
+export const offers = [
+  {
+    "bedrooms": 3,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": "Amsterdam"
+    },
+    "description": "A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.",
+    "goods": [
+      "Heating"
+    ],
+    "host": {
+      "avatarUrl": "img/1.png",
+      "id": 3,
+      "isPro": true,
+      "name": "Angelina"
+    },
+    "id": 1,
+    "images": [
+      "img/1.png"
+    ],
+    "isFavorite": true,
+    "isPremium": false,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "maxAdults": 4,
+    "previewImage": "img/1.png",
+    "price": 120,
+    "rating": 4.8,
+    "title": "Beautiful & luxurious studio at great location",
+    "type": "apartment"
+  },
+  {
+    "bedrooms": 1,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": "Amsterdam"
+    },
+    "description": "A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.",
+    "goods": [
+      "Heating"
+    ],
+    "host": {
+      "avatarUrl": "img/2.png",
+      "id": 4,
+      "isPro": false,
+      "name": "Denis"
+    },
+    "id": 2,
+    "images": [
+      "img/2.png"
+    ],
+    "isFavorite": true,
+    "isPremium": true,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "maxAdults": 1,
+    "previewImage": "img/2.png",
+    "price": 50,
+    "rating": 3.9,
+    "title": "Beautiful & luxurious studio at great location",
+    "type": "apartment"
+  },
+  {
+    "bedrooms": 2,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": "Amsterdam"
+    },
+    "description": "A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.",
+    "goods": [
+      "Heating"
+    ],
+    "host": {
+      "avatarUrl": "img/3.png",
+      "id": 5,
+      "isPro": true,
+      "name": "Mark"
+    },
+    "id": 3,
+    "images": [
+      "img/3.png"
+    ],
+    "isFavorite": false,
+    "isPremium": true,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "maxAdults": 3,
+    "previewImage": "img/3.png",
+    "price": 135,
+    "rating": 4.1,
+    "title": "Beautiful & luxurious studio at great location",
+    "type": "apartment"
+  },
+  {
+    "bedrooms": 1,
+    "city": {
+      "location": {
+        "latitude": 52.370216,
+        "longitude": 4.895168,
+        "zoom": 10
+      },
+      "name": "Amsterdam"
+    },
+    "description": "A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.",
+    "goods": [
+      "Heating"
+    ],
+    "host": {
+      "avatarUrl": "img/1.png",
+      "id": 6,
+      "isPro": true,
+      "name": "Ivan"
+    },
+    "id": 4,
+    "images": [
+      "img/1.png"
+    ],
+    "isFavorite": true,
+    "isPremium": false,
+    "location": {
+      "latitude": 52.35514938496378,
+      "longitude": 4.673877537499948,
+      "zoom": 8
+    },
+    "maxAdults": 3,
+    "previewImage": "img/1.png",
+    "price": 99,
+    "rating": 2.7,
+    "title": "Beautiful & luxurious studio at great location",
+    "type": "apartment"
+  }
+];
