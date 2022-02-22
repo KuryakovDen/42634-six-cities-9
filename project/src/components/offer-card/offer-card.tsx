64be7290f694410991cb5d00ef0b2a6aebb1c981
dkuryakov <fifer2@yandex.ru>
2022-02-23 00:39:50 +0700

    feat: Отрисовка карточек предложений согласно мокам

--- project/src/components/offer-card/offer-card.tsx
+++ project/src/components/offer-card/offer-card.tsx
@@ -6,23 +6,28 @@ type OfferCardProps = {
 };
 
 function OfferCard({ offer }: OfferCardProps): JSX.Element {
+  const { description, type, price, previewImage, isFavorite, isPremium, rating } = offer;
+  const roundedRating = Math.round(rating);
+
   return (
     <article className="cities__place-card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>s
+      {isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      )}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src={'image1.src'} width="260" height="200" alt="Place image" />
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{1}</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className={`${isFavorite ? 'place-card__bookmark-button--active' : 'place-card__bookmark-button'} button`} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark" />
             </svg>
@@ -31,13 +36,15 @@ function OfferCard({ offer }: OfferCardProps): JSX.Element {
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
+            {roundedRating}
+            <span style={{width: `${roundedRating * 20}%`}} />
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">{description}</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>
   );
