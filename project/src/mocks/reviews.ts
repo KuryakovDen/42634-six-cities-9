11dd3a5ececf83c7e7d893731127437833f3cf31
dkuryakov <fifer2@yandex.ru>
2022-02-23 12:18:17 +0700

    feat: Создание моков для комментариев

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,52 @@
+import {Review} from '../types/review';
+
+export const reviews: Review[] = [
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': 'Wed Feb 11 2022 12:10:51 GMT+0700 (Novosibirsk Standard Time)',
+    'id': 1,
+    'rating': 5,
+    'user': {
+      'avatarUrl': 'img/2.png',
+      'id': 1,
+      'isPro': true,
+      'name': 'Adebayo.Akinvenwa'
+    }
+  },
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': 'Wed Feb 23 2022 12:10:51 GMT+0700 (Novosibirsk Standard Time)',
+    'id': 2,
+    'rating': 4,
+    'user': {
+      'avatarUrl': 'img/1.png',
+      'id': 2,
+      'isPro': false,
+      'name': 'Denis.Kuryakov'
+    }
+  },
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': 'Wed Feb 20 2022 12:10:51 GMT+0700 (Novosibirsk Standard Time)',
+    'id': 3,
+    'rating': 2,
+    'user': {
+      'avatarUrl': 'img/1.png',
+      'id': 3,
+      'isPro': false,
+      'name': 'Ivan.Ivanov'
+    }
+  },
+  {
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'date': 'Wed Feb 15 2022 12:10:51 GMT+0700 (Novosibirsk Standard Time)',
+    'id': 4,
+    'rating': 4,
+    'user': {
+      'avatarUrl': 'img/1.png',
+      'id': 4,
+      'isPro': true,
+      'name': 'Oliver.conner'
+    }
+  }
+];
