324c8b5136dd6b1de9d96243cd1a94406fb3b537
dkuryakov <fifer2@yandex.ru>
2022-03-09 21:49:30 +0300

    refactor: Рефакторинг

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import {offers} from './mocks/offers';
 import {reviews} from './mocks/reviews';
 import {Provider} from 'react-redux';
 import {store} from './store';
@@ -9,7 +8,7 @@ import {store} from './store';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App offers={offers} reviews={reviews} />
+      <App reviews={reviews} />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
