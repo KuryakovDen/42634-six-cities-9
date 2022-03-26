318fc011f90c67e21e515ee9d1f50f6d2f5cf358
dkuryakov <fifer2@yandex.ru>
2022-03-26 21:33:46 +0300

    feat: Рендеринг компонента при отсутствии предложений

--- project/src/screens/main-screen/main-screen.tsx
+++ project/src/screens/main-screen/main-screen.tsx
@@ -6,7 +6,6 @@ import OffersList from '../../components/offers-list/offers-list';
 import {Offer, OfferLocation} from '../../types/offer';
 import Map from '../../components/map/map';
 import {useAppSelector} from '../../hooks';
-import Spinner from '../../components/spinner/spinner';
 
 function MainScreen(): JSX.Element {
   const [activeOfferLocation, setActiveOfferLocation] = useState<null | OfferLocation>(null);
@@ -59,7 +58,25 @@ function MainScreen(): JSX.Element {
       </div>
     )
   ) : (
-    <Spinner />
+    <div className="page page--gray page--main">
+      <Header />
+
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <LocationTabs />
+        <div className="cities">
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">No places to stay available</b>
+                <p className="cities__status-description">We could not find any property available at the moment in {activeLocation}</p>
+              </div>
+            </section>
+            <div className="cities__right-section"></div>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
 
