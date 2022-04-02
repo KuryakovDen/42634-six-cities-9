1e2b48ad7e9655978a9a5e3ced8e69add522a3cc
dkuryakov <fifer2@yandex.ru>
2022-04-02 12:30:57 +0300

    feat: Доработка пустого списка городов

--- project/src/screens/main-screen/main-screen.tsx
+++ project/src/screens/main-screen/main-screen.tsx
@@ -81,7 +81,7 @@ function MainScreen(): JSX.Element {
                 <p className="cities__status-description">We could not find any property available at the moment in {activeLocation}</p>
               </div>
             </section>
-            <div className="cities__right-section"></div>
+            <div className="cities__right-section" style={{backgroundImage: 'url(../img/no-places@2x.png)'}}/>
           </div>
         </div>
       </main>
